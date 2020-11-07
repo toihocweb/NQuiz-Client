e2a30c1f20082f2b9c4658b1465a6bff81251d1c
longnh <longnguyenhoang91@gmail.com>
2020-11-07 22:44:42 +0700

    confirm email success page

--- pages/register/confirmation.tsx
+++ pages/register/confirmation.tsx
@@ -15,7 +15,12 @@ const RegisterConfirmPage = () => {
           justify="center"
           align="middle"
         >
-          <Col style={{ textAlign: "center" }}>
+          <Col style={{ textAlign: "center", marginTop: "20px" }}>
+            <img
+              style={{ maxWidth: "150px" }}
+              src="/images/logo/nquiz_logo_xs.png"
+              alt="logo"
+            />
             <RegisterConfirm />
           </Col>
         </Row>
